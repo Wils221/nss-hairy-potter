1c49657a175a8036d39f05b77d0f7fddd68e57d8
Wil Hammond <wil.hammond@gmail.com>
2022-10-24 15:30:35 -0500

    1st

--- src/scripts/PotteryWheel.js
+++ src/scripts/PotteryWheel.js
@@ -1,12 +1,11 @@
 const primaryKey = 1;
 
 export const makePottery = (shape, weight, height) => {
-    let pots = {};
-    pots.shape = shape;
-    pots.weight = weight;
-    pots.height = height;
-    pots.id = primaryKey
-    
+    let pots = {}
+    shape = shape;
+    weight = weight;
+    height = height;
+    id = primaryKey++   
 
     return pots
 }
